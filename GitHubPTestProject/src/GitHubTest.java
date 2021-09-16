ad83e2f61136f227bc6aa93ba72a56f98efcc18b
chaitrapatil555 <pchaitra555@gmail.com>
2021-09-16 14:30:58 +0530

    first commit

---
+++ GitHubPTestProject/src/GitHubTest.java
@@ -0,0 +1,13 @@
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String code = "SE";
+if(code == "SE")
+	System.out.println("software engineer");
+else
+	System.out.println("not a software engineer");
+	}
+
+}
